9f8583d33756bc2944d88fcf0998205e0cf38a57
Luke Tchang <ltchang@stanford.edu>
2020-09-10 16:03:35 -0700

    added cookie session and jwt token to signup route

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 import { User } from '../models/user';
+import jwt from 'jsonwebtoken';
 
 import { RequestValidationError } from '../errors/request-validation-error';
 import { DatabaseConnectionError } from '../errors/database-connection-error';
@@ -32,6 +33,15 @@ router.post('/api/users/sign-up', [
 
         const newUser = User.build({ email, password });
         await newUser.save();
+
+        const userJwt = jwt.sign({
+            id: newUser.id,
+            email: newUser.email
+        }, 'SECRET_KEY');
+
+        req.session = {
+            jwt: userJwt
+        };
 
         res.status(201).send(newUser);
     }
