af84aa7c014330817366bdd1fdd1b4cdad7fbad3
Luke Tchang <ltchang@stanford.edu>
2020-09-11 09:25:45 -0700

    started signin route, extracted request validation into middleware

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,25 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+
+import { validateRequest } from '../middlewares/validate-request';
+import { RequestValidationError } from '../errors/request-validation-error';
 
 const router = express.Router();
 
-router.post('/api/users/sign-in', (req, res) => {
-    res.send('Sign In');
-});
+router.post('/api/users/sign-in', 
+    [
+        body('email')
+            .isEmail()
+            .withMessage('Please enter valid email.'),
+        body('password')
+            .trim()
+            .notEmpty()
+            .withMessage('Must supply a password')
+    ],
+    validateRequest,
+    (req: Request, res: Response) => {
+
+    }
+);
 
 export { router as signinRouter };
