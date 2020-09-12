78157fdd2d9322d270de2b4809be67f0503b0fe8
Luke Tchang <ltchang@stanford.edu>
2020-09-11 21:00:00 -0700

    moved current-user logic to middleware

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,7 +3,8 @@ import express from 'express';
 const router = express.Router();
 
 router.post('/api/users/sign-out', (req, res) => {
-    res.send('Sign Out');
+    req.session = null;
+    res.send('Signed out.');
 });
 
 export { router as signoutRouter };
