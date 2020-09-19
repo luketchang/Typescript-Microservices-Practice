19906d14c0fae41e73e225b0adebaa336bf9ba3e
Luke Tchang <ltchang@stanford.edu>
2020-09-19 11:58:50 -0700

    completed update route implementation and tests

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -49,14 +49,52 @@ it('returns 401 if user does not own ticket', async () => {
         .expect(401);
 });
 
-it('returns 404 if provided ticket id does not exist', async () => {
-    
-});
-
 it('returns 400 if user provides invalid title or price', async () => {
+    const cookie = getAuthCookie();
+    const title = 'title';
+    const price = 100;
+
+    const res = await createTicket(title, price, cookie);
+
+    await request(app)
+        .put(`/api/tickets/${res.body.id}`)
+        .set('Cookie', cookie)
+        .send({
+            title: '',
+            price: 150
+        })
+        .expect(400);
     
+    await request(app)
+        .put(`/api/tickets/${res.body.id}`)
+        .set('Cookie', cookie)
+        .send({
+            title: 'new title',
+            price: -10
+        })
+        .expect(400);
 });
 
 it('updates the ticket if all inputs valid', async () => {
+    const cookie = getAuthCookie();
+    const title = 'title';
+    const price = 100;
+
+    const res = await createTicket(title, price, cookie);
+
+    await request(app)
+        .put(`/api/tickets/${res.body.id}`)
+        .set('Cookie', cookie)
+        .send({
+            title: 'new title',
+            price: 200
+        })
+        .expect(200);
+
+    const getTicketRes = await request(app)
+        .get(`/api/tickets/${res.body.id}`)
+        .send();
     
+    expect(getTicketRes.body.title).toEqual('new title');
+    expect(getTicketRes.body.price).toEqual(200);
 });
