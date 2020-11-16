d9af1a5e8b46aea718fd8ff5b30d329880a8242d
Luke Tchang <ltchang@stanford.edu>
2020-11-16 11:44:17 -0800

    updates

---
+++ common/src/events/ticket-created-event.ts
@@ -0,0 +1,12 @@
+import { Subject } from './subject';
+import { Event } from './base-event';
+
+export interface TicketCreatedEvent extends Event {
+    subject: Subject.TicketCreated;
+    data: {
+        id: string;
+        title: string;
+        price: number;
+        userId: string;
+    }
+}
---
+++ common/src/events/ticket-updated-event.ts
@@ -0,0 +1,12 @@
+import { Subject } from './subject';
+import { Event } from './base-event';
+
+export interface TicketUpdatedEvent extends Event {
+    subject: Subject.TicketUpdated;
+    data: {
+        id: string;
+        title: string;
+        price: number;
+        userId: string;
+    }
+}
