ee8da2ed5e3c73a9708925ab98b2cf58fa3cfd46
Luke Tchang <ltchang@stanford.edu>
2020-11-16 10:22:27 -0800

    extracted abstract and ticket-created listener classes into separate files

---
+++ nats-test/src/events/ticket-created-listener.ts
@@ -0,0 +1,12 @@
+import { Listener } from './base-listener';
+import { Message } from 'node-nats-streaming';
+
+export class TicketCreatedListener extends Listener {
+    subject = 'ticket:created';
+    queueGroupName = 'payments-service';
+    
+    onMessage(data: any, msg: Message) {
+      console.log('Event data!', data);
+      msg.ack();
+    }
+  }
