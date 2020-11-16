4b0079d7691b8a7f0a8f6640f15bc6c99093584e
Luke Tchang <ltchang@stanford.edu>
2020-11-16 10:46:54 -0800

    extracted subject names to enum types, created custom event type for ticket-created, and created interface to couple custom events with subclassed listeners

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,8 +1,10 @@
 import { Listener } from './base-listener';
 import { Message } from 'node-nats-streaming';
+import { TicketCreatedEvent } from './ticket-created-event';
+import { Subject } from './subject';
 
-export class TicketCreatedListener extends Listener {
-    subject = 'ticket:created';
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+    readonly subject: Subject.TicketCreated = Subject.TicketCreated;
     queueGroupName = 'payments-service';
     
     onMessage(data: any, msg: Message) {
