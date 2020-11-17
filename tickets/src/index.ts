d2b96a5cd83ea2292492650d64fe2eb16c514a65
Luke Tchang <ltchang@stanford.edu>
2020-11-16 20:01:55 -0800

    extracted tickets service params for NATS connection to env variables

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -4,15 +4,19 @@ import { app } from './app';
 import { natsWrapper } from './nats-wrapper';
 
 const start = async () => {
-    if(!process.env.JWT_KEY) {
-        throw new Error('JWT_KEY not defined.');
-    }
-    if(!process.env.TICKETS_MONGO_URI) {
-        throw new Error('TICKETS_MONGO_URI not defined.')
-    }
+    if(!process.env.JWT_KEY) throw new Error('JWT_KEY not defined.');
+    if(!process.env.TICKETS_MONGO_URI) throw new Error('TICKETS_MONGO_URI not defined.')
+    if(!process.env.NATS_CLIENT_ID) throw new Error('NATS_CLIENT_ID not defined.');
+    if(!process.env.NATS_CLUSTER_ID) throw new Error('NATS_CLUSTER_ID not defined.');
+    if(!process.env.NATS_URL) throw new Error('NATS_URL not defined.');
 
     try {
-        await natsWrapper.connect('ticketing', 'randomstring', 'http://nats-srv:4222');
+        await natsWrapper.connect(
+            process.env.NATS_CLUSTER_ID, 
+            process.env.NATS_CLIENT_ID, 
+            process.env.NATS_URL
+        );
+        
         natsWrapper.client.on('close', () => {
             console.log('NATS connection closed!');
             process.exit();
