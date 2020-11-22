38262134d47e73ccd46f3a845cb64b604290adbe
Luke Tchang <ltchang@stanford.edu>
2020-11-22 00:34:19 -0800

    added order-cancelled event listeners to tickets srv + tests

--- tickets/src/events/listeners/__test__/order-cancelled-listener.test.ts
+++ tickets/src/events/listeners/__test__/order-cancelled-listener.test.ts
@@ -2,31 +2,29 @@ import { natsWrapper } from '../../../nats-wrapper';
 import mongoose, { mongo } from 'mongoose';
 
 import { Ticket } from '../../../models/ticket';
-import { OrderCreatedEvent, OrderStatus } from '@lt-ticketing/common';
-import { OrderCreatedListener } from '../order-created-listener';
+import { OrderCancelledEvent, OrderStatus } from '@lt-ticketing/common';
+import { OrderCancelledListener } from '../order-cancelled-listener';
 import { updateTicketRouter } from '../../../routes/update';
 
 const setup = async () => {
-    const listener = new OrderCreatedListener(natsWrapper.client);
+    const listener = new OrderCancelledListener(natsWrapper.client);
 
+    const orderId = mongoose.Types.ObjectId().toHexString();
     const ticket = Ticket.build({
         title: 'concert',
         price: 20,
         userId: mongoose.Types.ObjectId().toHexString()
     });
+    ticket.set({ orderId });
     await ticket.save();
 
-    const data: OrderCreatedEvent['data'] = {
-        id: mongoose.Types.ObjectId().toHexString(),
-        status: OrderStatus.Created,
-        userId: mongoose.Types.ObjectId().toHexString(),
-        expiresAt: 'expiretime',
+    const data: OrderCancelledEvent['data'] = {
+        id: orderId,
         version: 0,
         ticket: {
-            id: ticket.id,
-            price: ticket.price
+            id: ticket.id
         }
-    }
+    };
 
     //@ts-ignore
     const msg: Message = {
@@ -42,7 +40,7 @@ it('updates the ticket order id property', async () => {
     await listener.onMessage(data, msg);
     const updatedTicket = await Ticket.findById(ticket.id);
 
-    expect(updatedTicket!.orderId).toEqual(data.id);
+    expect(updatedTicket!.orderId).not.toBeDefined();
 });
 
 it('acks the message', async () => {
@@ -61,5 +59,5 @@ it('publishes a TicketUpdatedEvent to update version #s', async () => {
     const updatedTicketData = JSON.parse(
         (natsWrapper.client.publish as jest.Mock).mock.calls[0][1]
     );
-    expect(updatedTicketData.orderId).toEqual(data.id);
+    expect(updatedTicketData.orderId).toEqual(undefined);
 });
