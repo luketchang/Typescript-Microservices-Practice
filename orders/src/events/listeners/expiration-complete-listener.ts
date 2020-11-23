256eadd408b26571c60531133d1f95096c33b93d
Luke Tchang <ltchang@stanford.edu>
2020-11-22 17:32:30 -0800

    added expiration-complete listener to orders srv to update expired orders

---
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -0,0 +1,30 @@
+import { Message } from 'node-nats-streaming';
+import { Subject, QueueGroupName, ExpirationCompleteEvent, Listener, OrderStatus } from '@lt-ticketing/common';
+import { OrderCancelledPublisher } from '../publishers/order-cancelled-publisher';
+import { Order } from '../../models/order';
+
+export class TicketCreatedListener extends Listener<ExpirationCompleteEvent> {
+    subject: Subject.ExpirationComplete = Subject.ExpirationComplete;
+    queueGroupName = QueueGroupName.OrdersService;
+
+    async onMessage(data: ExpirationCompleteEvent['data'], msg: Message) {
+        const order  = await Order.findById(data.orderId);
+
+        if(!order) {
+            throw new Error('Order not found.');
+        }
+
+        order.set({ status: OrderStatus.Cancelled });
+        await order.save();
+
+        new OrderCancelledPublisher(this.client).publish({
+            id: order.id,
+            version: order.version,
+            ticket: {
+                id: order.ticket.id
+            }
+        });
+        
+        msg.ack();
+    }
+}
