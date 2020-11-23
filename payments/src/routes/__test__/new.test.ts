008689f498db5a54fc7afbcbaba901b76f86a1aa
Luke Tchang <ltchang@stanford.edu>
2020-11-23 12:02:03 -0800

    added mock stripe test to new route test

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -1,9 +1,12 @@
 import request from 'supertest';
 import mongoose from 'mongoose';
+import { stripe } from '../../stripe';
 import { app } from '../../app';
 import { Order } from '../../models/order';
 import { getAuthCookie } from '../../test/getAuthCookie';
 import { OrderStatus } from '@lt-ticketing/common';
+
+jest.mock('../../stripe');
 
 it('returns a 404 (NotFoundError) when order does not exist', async () => {
     const userId = mongoose.Types.ObjectId().toHexString();
@@ -61,6 +64,34 @@ it('returns 400 (BadRequestError) when purchasing cancelled order', async () => 
         .send({
             token: 'token',
             orderId: order.id
-        })
+        }) 
         .expect(400);
+});
+
+it('returns a 201 (created) with valid inputs', async () => {
+    const userId = mongoose.Types.ObjectId().toHexString();
+
+    const order = Order.build({
+        id: mongoose.Types.ObjectId().toHexString(),
+        price: 20,
+        userId,
+        status: OrderStatus.Created,
+        version: 0
+    });
+    await order.save();
+
+    const cookie = getAuthCookie(userId);
+    await request(app)
+        .post('/api/payments')
+        .set('Cookie', cookie)
+        .send({
+            orderId: order.id,
+            token: 'tok_visa'
+        })
+        .expect(201);
+
+    const chargeOptions = (stripe.charges.create as jest.Mock).mock.calls[0][0];
+    expect(chargeOptions.source).toEqual('tok_visa');
+    expect(chargeOptions.amount).toEqual(20 * 100);
+    expect(chargeOptions.currency).toEqual('usd');
 });
--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -9,6 +9,7 @@ import {
     OrderStatus
 } from '@lt-ticketing/common';
 import { Order } from '../models/order';
+import { stripe } from '../stripe';
 
 const router = express.Router();
 
@@ -39,7 +40,13 @@ router.post(
             throw new BadRequestError('Cannot pay for cancelled order.')
         }
 
-        res.send({ success: true });
+        await stripe.charges.create({
+            currency: 'usd',
+            amount: order.price * 100,
+            source: token
+        });
+
+        res.status(201).send({ success: true });
     }
 );
 
