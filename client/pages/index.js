93ed7b58542e5eb0f49cd3075b97566ef7ccab86
Luke Tchang <ltchang@stanford.edu>
2025-02-18 21:25:29 -0800

    save ported client

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,14 +1,41 @@
-import buildClient from '../api/build-client';
+import Link from 'next/link';
 
-const Landing = ({ currentUser }) => {
-    return currentUser ? (<h1>You are signed in.</h1>) : (<h1>Please sign in.</h1>);
-}
+const LandingPage = ({ currentUser, tickets }) => {
+  const ticketList = tickets.map((ticket) => {
+    return (
+      <tr key={ticket.id}>
+        <td>{ticket.title}</td>
+        <td>{ticket.price}</td>
+        <td>
+          <Link href="/tickets/[ticketId]" as={`/tickets/${ticket.id}`}>
+            View
+          </Link>
+        </td>
+      </tr>
+    );
+  });
 
-Landing.getInitialProps = async (context) => {
-    const client = buildClient(context);
-    const { data } = await client.get('/api/users/currentuser');
-    console.log(data.currentUser);
-    return data;
-}
+  return (
+    <div>
+      <h1>Tickets</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Link</th>
+          </tr>
+        </thead>
+        <tbody>{ticketList}</tbody>
+      </table>
+    </div>
+  );
+};
 
-export default Landing;
+LandingPage.getInitialProps = async (context, client, currentUser) => {
+  const { data } = await client.get('/api/tickets');
+
+  return { tickets: data };
+};
+
+export default LandingPage;
