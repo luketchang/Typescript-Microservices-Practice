a4680a7de2afa8c4b7c932c0220436b1443b3ba4
Luke Tchang <ltchang@stanford.edu>
2025-02-21 13:11:02 -0800

    remove node modules

--- nats-test/node_modules/@lt-ticketing/common/build/errors/custom-error.d.ts
+++
@@ -1,8 +0,0 @@
-export declare abstract class CustomError extends Error {
-    abstract statusCode: number;
-    abstract serializeErrors(): {
-        message: string;
-        field?: string;
-    }[];
-    constructor(message: string);
-}
